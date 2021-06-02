32393ed6d4335ba986e5f5199aa92a11839bd5f2
Jay T <jaytula@gmail.com>
2021-06-02 14:12:33 -0700

    Finish 'Logical calculator'

---
+++ 8kyu/logical-calculator.test.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * @param {boolean[]} array
+ * @param {string} op
+ * @returns
+ */
+function logicalCalc(array, op) {
+  return array
+    .slice(1)
+    .reduce(
+      (acc, curr) =>
+        (acc = { AND: acc && curr, OR: acc || curr, XOR: acc ? !curr : curr }[
+          op
+        ]),
+      array[0]
+    );
+}
+
+it("Fixed Tests", function() {
+  expect(logicalCalc([true, true, true, false], "AND")).toBe(false);
+  expect(logicalCalc([true, true, true, false], "OR")).toBe(true);
+  expect(logicalCalc([true, true, true, false], "XOR")).toBe(true);
+  expect(logicalCalc([true, true, false, false], "AND")).toBe(false);
+  expect(logicalCalc([true, true, false, false], "OR")).toBe(true);
+  expect(logicalCalc([true, true, false, false], "XOR")).toBe(false);
+  expect(logicalCalc([true, false, false, false], "AND")).toBe(false);
+  expect(logicalCalc([true, false, false, false], "OR")).toBe(true);
+  expect(logicalCalc([true, false, false, false], "XOR")).toBe(true);
+  expect(logicalCalc([true, true], "AND")).toBe(true);
+  expect(logicalCalc([true, true], "OR")).toBe(true);
+  expect(logicalCalc([true, true], "XOR")).toBe(false);
+  expect(logicalCalc([false, false], "AND")).toBe(false);
+  expect(logicalCalc([false, false], "OR")).toBe(false);
+  expect(logicalCalc([false, false], "XOR")).toBe(false);
+  expect(logicalCalc([false], "AND")).toBe(false);
+  expect(logicalCalc([false], "OR")).toBe(false);
+  expect(logicalCalc([false], "XOR")).toBe(false);
+  expect(logicalCalc([true], "AND")).toBe(true);
+  expect(logicalCalc([true], "OR")).toBe(true);
+  expect(logicalCalc([true], "XOR")).toBe(true);
+});
