7c2f22ab66655d818d14646a853024001bc2ac93
Jay T <jaytula@gmail.com>
2021-06-07 15:24:00 -0700

    Complete 'Removing Elements'

---
+++ test/removing-elements.test.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @param {string[]} arr 
+ */
+function removeEveryOther(arr){
+  let current = 0;
+  while(current < arr.length) {
+    arr.splice(current+1, 1);
+    current++
+  }
+  return arr;
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+    assert.deepEqual(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+    assert.deepEqual(removeEveryOther([[1, 2]]), [[1, 2]]);
+    assert.deepEqual(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
+  })
+});
