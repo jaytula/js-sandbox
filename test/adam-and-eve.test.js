7ea49726295ba5b208c53537db23be72cd39707b
Jay T <jaytula@gmail.com>
2021-06-09 12:10:05 -0700

    Complete 'Basic subclasses - Adam and Eve'

---
+++ test/adam-and-eve.test.js
@@ -0,0 +1,35 @@
+class Human {}
+
+class Man extends Human {
+  constructor() {
+    super();
+  }
+}
+
+class Woman extends Human {
+  constructor() {
+    super();
+  }
+}
+
+class God {
+  /**
+   * @returns Human[]
+   */
+  static create() {
+    return [new Man(), new Woman()];
+  }
+}
+
+const Test = require("@codewars/test-compat");
+
+describe("GodTest", function () {
+  it("Adam should be a Man", function () {
+    let humans = God.create();
+    Test.assertEquals(
+      humans[0] instanceof Man,
+      true,
+      "Expected Adam to be a Man"
+    );
+  });
+});
