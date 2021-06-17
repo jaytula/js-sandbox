398afb7797225ae6c78a7b47c21bd1f47897753d
Jay T <jaytula@gmail.com>
2021-06-16 23:25:49 -0700

    Complete 'Doing something n times'

---
+++ test/doing-something-n-times.test.js
@@ -0,0 +1,35 @@
+
+Number.prototype.times = function (f) {
+  for(let i = 0; i < this; i++) {
+    try {
+     f(i);
+    } catch(err) {
+    }
+  }
+};
+
+const Test = require("@codewars/test-compat");
+
+describe("Number.prototype.times", () => {
+  it("should work exactly like a for loop and throw no errors", () => {
+    Test.expectNoError("Unexpected error was thrown", (_) => {
+      (6).times((_) => {
+        console.log("Hello World");
+      });
+      (6).times((i) => {
+        if (typeof i !== "number")
+          throw "'i' was not properly supplied to the anonymous function ;)";
+        console.log(i);
+      });
+    });
+    console.log("<h3>Same as doing this:</h3>");
+    for (let i = 0; i < 6; i++) {
+      console.log("Hello World");
+    }
+    for (let i = 0; i < 6; i++) {
+      if (typeof i !== "number")
+        throw "'i' was not properly supplied to the anonymous function ;)";
+      console.log(i);
+    }
+  });
+});
