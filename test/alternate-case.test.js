f2e4d03afb74322c15a07f01bd9ff929a66180f1
Jay T <jaytula@gmail.com>
2021-06-20 01:09:03 -0700

    Complete 'Alternate case'

---
+++ test/alternate-case.test.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param {string} s
+ * @returns
+ */
+function alternateCase(s) {
+  return s
+    .split("")
+    .map((ch) =>
+      ch === ch.toLowerCase() ? ch.toUpperCase() : ch.toLowerCase()
+    )
+    .join("");
+}
+
+const Test = require("@codewars/test-compat");
+
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(alternateCase("abc"), "ABC");
+    Test.assertEquals(alternateCase("ABC"), "abc");
+    Test.assertEquals(alternateCase("Hello World"), "hELLO wORLD");
+  });
+});
