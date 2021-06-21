9f30de8ebbefa5cb3a6bb03e6a6b114e40d89159
Jay T <jaytula@gmail.com>
2021-06-21 14:53:50 -0700

    Complete 'Guess the Hat'

---
+++ test/guess-hat-color.test.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {string} a 
+ * @param {string} b 
+ * @param {string} c 
+ * @param {string} d 
+ */
+function guessHatColor(a,b,c,d) {
+  return b === c ? 1 : 2;
+}
+
+const Test = require('@codewars/test-compat');
+
+describe("Basic Tests", function() {
+  it("It should works for basic tests.", function() {
+  
+    Test.assertEquals(guessHatColor("black","white","black","white"),2)
+    
+    Test.assertEquals(guessHatColor("white","black","white","black"),2)
+    
+    Test.assertEquals(guessHatColor("white","black","black","white"),1)
+  
+  })})
