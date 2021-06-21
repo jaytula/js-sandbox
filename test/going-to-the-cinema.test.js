f48c2ae838f4f05012dcaafe14066e6c828836df
Jay T <jaytula@gmail.com>
2021-06-21 00:07:30 -0700

    Complete 'Going  to the cinema'

--- test/going-to-the-cinema.test.js
+++ test/going-to-the-cinema.test.js
@@ -10,10 +10,10 @@ function movie(card, ticket, perc) {
   let systemATotal = 0;
   let systemBTotal = card;
 
-  while (systemBTotal > systemATotal) {
+  while (Math.ceil(systemBTotal) >= systemATotal) {
     numberOfTimes++;
     systemATotal += ticket;
-    systemBTotal += ticket * perc ** numberOfTimes;
+    systemBTotal += ticket * perc**numberOfTimes;
   }
   return numberOfTimes;
 }
@@ -24,5 +24,6 @@ describe("Movie", function () {
   it("Basic tests", function () {
     Test.assertEquals(movie(500, 15, 0.9), 43);
     Test.assertEquals(movie(100, 10, 0.95), 24);
+    Test.assertEquals(movie(107662, 3, 0.32), 35889)
   });
 });
