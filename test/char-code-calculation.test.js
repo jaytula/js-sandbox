355a86ea5ca382267f111f85a423f3988bd48968
Jay T <jaytula@gmail.com>
2021-06-21 19:08:10 -0700

    Complete 'Char Code Calculation'

---
+++ test/char-code-calculation.test.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @param {string} s
+ */
+function calc(s) {
+  return s
+    .split("")
+    .reduce(
+      (acc, ch) =>
+        acc + ch.charCodeAt(0).toString().replace(/[^7]/g, "").length * 6,
+      0
+    );
+}
+
+const Test = require("@codewars/test-compat");
+
+describe("Example tests", () => {
+  test("test", () => {
+    Test.assertEquals(calc("ABC"), 6);
+    Test.assertEquals(calc("abcdef"), 6);
+    Test.assertEquals(calc("ifkhchlhfd"), 6);
+    Test.assertEquals(calc("aaaaaddddr"), 30);
+    Test.assertEquals(
+      calc("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+      96
+    );
+  });
+});
