8cdf693831a8dcc4cbf315f3c5ba5969ec72172c
Jay T <jaytula@gmail.com>
2021-06-22 23:48:31 -0700

    Complete 'Birthday I - Cake'

---
+++ test/birthday-i-cake.test.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {number} totalCandles
+ * @param {string} s
+ */
+function cake(totalCandles, s) {
+  const fallenCandles = s.split("").reduce((acc, ch, idx) => {
+    return (
+      acc +
+      (idx % 2 === 0 ? ch.charCodeAt(0) : ch.charCodeAt(0) - "a".charCodeAt(0) + 1)
+    );
+  }, 0);
+  console.log({fallenCandles, totalCandles, percent: fallenCandles / totalCandles})
+  return fallenCandles / totalCandles > 0.7 ? "Fire!" : "That was close!";
+}
+
+const Test = require("@codewars/test-compat");
+
+describe("Example tests", () => {
+  it("test", () => {
+    Test.assertEquals(cake(900, "abcdef"), "That was close!");
+    Test.assertEquals(cake(56, "ifkhchlhfd"), "Fire!");
+    Test.assertEquals(cake(256, "aaaaaddddr"), "Fire!");
+    Test.assertEquals(cake(488, "nlhkib"), "Fire!");
+
+  });
+});
