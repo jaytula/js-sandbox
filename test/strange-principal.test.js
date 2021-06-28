71a2cb8842af431afe6f9e26d9d2a34d1a6e8e7d
Jay T <jaytula@gmail.com>
2021-06-28 14:06:20 -0700

    Complete 'Strange Principal'

--- test/strange-principal.test.js
+++ test/strange-principal.test.js
@@ -5,16 +5,7 @@
  */
 function numOfOpenLockers(n) {
   if (n <= 1) return n;
-  let total = 1;
-  for (let i = 2; i <= n; i++) {
-    let open = false;
-    for (let j = i; j <= Math.sqrt(n); j += i) {
-      if(i % j === 0) open = !open;
-    }
-    total += +open;
-  }
-
-  return total;
+  return Math.floor(Math.sqrt(n))
 }
 
 const Test = require("@codewars/test-compat");
