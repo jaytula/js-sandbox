3cc825e4f7e7e37297c685618f57f6eca7b52c4b
Jay T <jaytula@gmail.com>
2021-06-30 22:48:13 -0700

    Complete 'Chuck Norris IV - Bearded Fist'

---
+++ test/chuck-norris-iv-bearded-fist.test.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @param {number[][]} arr
+ */
+function fistBeard(arr) {
+  return arr
+    .reduce((acc, curr) => acc.concat(curr), [])
+    .map((num) => String.fromCharCode(num))
+    .join("");
+}
+
+const Test = require("@codewars/test-compat");
+
+test("Example tests", () => {
+  Test.assertEquals(
+    fistBeard([[78], [117, 110, 99], [104, 117], [107, 115]]),
+    "Nunchuks"
+  );
+  Test.assertEquals(
+    fistBeard([
+      [70, 97, 99],
+      [101, 45, 75, 105, 99, 107],
+    ]),
+    "Face-Kick"
+  );
+});
