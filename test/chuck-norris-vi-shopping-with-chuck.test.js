ccb7445d0087ca9cc349405912a97d3a4662ac8b
Jay T <jaytula@gmail.com>
2021-07-02 17:39:37 -0700

    Complete 'Chuck Norris VI - Shopping with Chuck'

--- test/chuck-norris-vi-shopping-with-chuck.test.js
+++ test/chuck-norris-vi-shopping-with-chuck.test.js
@@ -1,10 +1,24 @@
+const SOIL_MAP = {
+  "Barely used": 10,
+  "Seen a few high kicks": 25,
+  "Blood stained": 30,
+  "Heavily soiled": 50,
+};
 function price(start, soil, age) {
-  // ACTION PANTS!!
+  if (
+    typeof start !== "number" ||
+    typeof soil !== "string" ||
+    !(soil in SOIL_MAP) || 
+    typeof age !== "number"
+  )
+    return "Chuck is bottomless!";
+
+  return "$" + (start * ((100 + SOIL_MAP[soil]) / 100) ** age).toFixed(2);
 }
 
 const Test = require("@codewars/test-compat");
 
-it("Example tests", () => {
+it("Chuck Norris VI - Shopping with Chuck", () => {
   Test.assertEquals(price(19.95, "Barely used", 3), "$26.55");
   Test.assertEquals(price(27.76, "Seen a few high kicks", 15), "$788.99");
   Test.assertEquals(price(13.26, "Blood stained", 25), "$9356.80");
